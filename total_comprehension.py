1f36d51fc6673e2ed572918355694270fbdc634e
minipele06 <aja398@stern.nyu.edu>
2020-05-28 15:58:05 -0400

    Update total_comprehension.py

--- total_comprehension.py
+++ total_comprehension.py
@@ -1,39 +1,25 @@
 # total_comprehension.py
 
-def great_three(i):
-    return i > 3
-
-def great_eight(i):
-    return i > 8
-
 my_numbers = [1, 2, 3, 4, 5, 6, 7]
+my_list = [n * 100 for n in my_numbers]
+my_list2 = [n for n in my_numbers if n > 3]
+my_list3 = [n for n in my_numbers if n > 8]
+my_list4 = [n * 100 for n in my_numbers if n > 3]
+
 print("--------------")
 print("ORIGINAL LIST:", my_numbers)
 
 print("--------------")
 print("TOTAL COMPREHENSION...")
 
-# TODO: write python code here
-
-my_list = []
-my_list2 = []
-
-for i in my_numbers:
-    my_list.append(i*100)
-
-great3_list = list(filter(great_three,my_numbers))
-great8_list = list(filter(great_eight,my_numbers))
-
-for i in great3_list:
-    my_list2.append(i*100)
-
 print("--------------")
 print("MAPPED LIST:", my_list)
 
 print("--------------")
-print("FILTERED LIST W/ MATCHES:", great3_list)
+print("FILTERED LIST W/ MATCHES:", my_list2)
 
 print("--------------")
-print("FILTERED LIST W/O MATCHES:", great8_list)
+print("FILTERED LIST W/O MATCHES:", my_list3)
+
 print("--------------")
-print("MAPPED AND FILTERED LIST:", my_list2)
+print("MAPPED AND FILTERED LIST:", my_list4)
